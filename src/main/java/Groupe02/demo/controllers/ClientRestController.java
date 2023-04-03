69e17ea1d2e00a71a5b8279db6312d7db2a312e8
CodeurTom <agnekitomm@gmail.com>
2023-04-03 16:56:22 +0200

    Client Compte

--- src/main/java/Groupe02/demo/controllers/ClientRestController.java
+++ src/main/java/Groupe02/demo/controllers/ClientRestController.java
@@ -1,7 +1,50 @@
 package Groupe02.demo.controllers;
 
+import Groupe02.demo.entities.Client;
+import Groupe02.demo.services.ClientService;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @RestController
 public class ClientRestController {
+    public class ClientController {
+        @Autowired
+        ClientService cs;
+
+        @PostMapping("clients")
+        public Client saveClient(@RequestBody Client client) {
+            return cs.saveClient(client);
+        }
+
+        //@GetMapping("clients/{id}")
+        //public Client getOneClient(@PathVariable Long id) {
+        //  return cs.saveClient(id);
+        //}
+
+        //@GetMapping("clients")
+        //public List<Client> showClient() {
+        //  return cs.showClient();
+        //}
+
+        //@DeleteMapping("clients/{id}")
+        //public void deleteClient(@PathVariable Long id) {
+        //cs.deleteClient(Long.valueOf(id));
+        //}
+
+        //@PutMapping("clients/{id}")
+        //public Client updateClient(@PathVariable Long id, @RequestBody Client client) {
+        //  client.setId(id);
+        //  return cs.saveClient(client);
+        //}
+    }
 }
--- src/main/java/Groupe02/demo/services/ClientService.java
+++ src/main/java/Groupe02/demo/services/ClientService.java
@@ -3,22 +3,31 @@ package Groupe02.demo.services;
 import Groupe02.demo.entities.Client;
 import Groupe02.demo.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Service
-public class ClientService implements ClientServiceImpl {
-    private ClientRepository clientRepository;
-    public List<Client> showClient() {
-        return ClientRepository.findAll();
-    }
+public class ClientService {
+    @Autowired
+    ClientRepository cr;
     public Client saveClient (Client client) {
-        return ClientRepository.save (client);
+        client.setId(Long.valueOf(client.getNom().substring(0, 2).toUpperCase()+
+                client.getPrenom().substring(0, 2).toUpperCase()+client.getDateNaissance().getYear()+
+                client.getSexe().substring(0, 2).toUpperCase()+client.getAdresse().substring(0, 2).toUpperCase()+
+                client.getTelephone().substring(0, 2).toUpperCase()+client.getCourriel().substring(0, 2).toUpperCase()+
+                client.getNationalite().substring(0, 2).toUpperCase()+client.getComptes().subList(0, 2)));
+
+        return cr.save(client);
+    }
+
+    public List<Client> showClient() {
+        return cr.findAll();
     }
     public Client getOneClient (Long id) {
-        return ClientRepository.findById(id).get();
+        return cr.findById(id).get();
     }
     public void deleteClient (Long id) {
-        ClientRepository.delete ById(id);
+       cr.deleteById(id);
     }
 }
